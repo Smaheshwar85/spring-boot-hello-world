86c86f1084a42fab8f3f7a3adbbf344660157d50
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2023-08-24 13:46:01 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/testing/HelloWorldApplication.java
+++ src/main/java/com/testing/HelloWorldApplication.java
@@ -3,7 +3,8 @@ package com.testing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.testing" })
+@EnableJpaAuditing
+@SpringBootApplication
 public class HelloWorldApplication {
     
     public static void main(String[] args) {
