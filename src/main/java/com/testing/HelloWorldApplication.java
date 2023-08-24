73f889144e0473ea2d850f08ed0d93549d47b3cb
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2023-08-24 14:34:36 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/testing/HelloWorldApplication.java
+++ src/main/java/com/testing/HelloWorldApplication.java
@@ -1,7 +1,8 @@
 package com.testing;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 
 @SpringBootApplication
